9ac6c270d00fe2b96e976145c199b02b11dd362f
AlexPet <alexandrpet195@gmail.com>
2025-04-21 19:20:10 +0300

    AH1 fix1

--- app/main.ts
+++ app/main.ts
@@ -1,40 +1,41 @@
-// Examples:
-// - decodeBencode("5:hello") -> "hello"
-// - decodeBencode("10:hello12345") -> "hello12345"
-function decodeBencode(
-  bencodedValue: string
-): string | number | (string | number)[] {
-  switch (bencodedValue[0]) {
-    case "i":
-      return parseInt(bencodedValue.substring(1, bencodedValue.length - 1));
-    case "l":
-      const list: (string | number)[] = [];
-      let index = 1;
-      while (bencodedValue[index] !== "e") {
-        if (bencodedValue[index] === "i") {
-          const end = bencodedValue.indexOf("e", index);
-          list.push(parseInt(bencodedValue.substring(index + 1, end)));
-          index = end + 1;
-        } else {
-          const colon = bencodedValue.indexOf(":", index);
+function decodeBencode(bencodedValue: string): any {
+  function parse(index: number): [any, number] {
+    const char = bencodedValue[index];
+
+    switch (char) {
+      case 'i': {
+        const end = bencodedValue.indexOf('e', index);
+        const number = parseInt(bencodedValue.substring(index + 1, end));
+        return [number, end + 1];
+      }
+
+      case 'l': {
+        const list: any[] = [];
+        index++; 
+        while (bencodedValue[index] !== 'e') {
+          const [value, nextIndex] = parse(index);
+          list.push(value);
+          index = nextIndex;
+        }
+        return [list, index + 1];
+      }
+      default: {
+        if (/\d/.test(char)) {
+          const colon = bencodedValue.indexOf(':', index);
           const length = parseInt(bencodedValue.substring(index, colon));
-          index = colon + 1;
-          list.push(bencodedValue.substring(index, index + length));
-          index += length;
+          const start = colon + 1;
+          const end = start + length;
+          const str = bencodedValue.substring(start, end);
+          return [str, end];
+        } else {
+          throw new Error(`Unexpected character: ${char}`);
         }
       }
-      return list;
-    default:
-      if (!isNaN(parseInt(bencodedValue[0]))) {
-        const firstColonIndex = bencodedValue.indexOf(":");
-        if (firstColonIndex === -1) {
-          throw new Error("Invalid encoded value");
-        }
-        return bencodedValue.substring(firstColonIndex + 1);
-      } else {
-        throw new Error("Only strings are supported at the moment");
-      }
+    }
   }
+
+  const [result] = parse(0);
+  return result;
 }
 
 const args = process.argv;
