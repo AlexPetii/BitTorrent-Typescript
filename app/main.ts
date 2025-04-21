b43fe87accfaf0ed40a1ef615d2f6cd9749669d9
AlexPet <alexandrpet195@gmail.com>
2025-04-21 16:37:42 +0300

    add decode number and format document

--- app/main.ts
+++ app/main.ts
@@ -1,31 +1,29 @@
 // Examples:
 // - decodeBencode("5:hello") -> "hello"
 // - decodeBencode("10:hello12345") -> "hello12345"
-function decodeBencode(bencodedValue: string): string {
-    /* This function is used to decode a bencoded string
-    The bencoded string is a string that is prefixed by the length of the string
-    **/
-
-    // Check if the first character is a digit
-    if (!isNaN(parseInt(bencodedValue[0]))) {
-        const firstColonIndex = bencodedValue.indexOf(":");
-        if (firstColonIndex === -1) {
-            throw new Error("Invalid encoded value");
-        }
-        return bencodedValue.substring(firstColonIndex + 1);
-    } else {
-        throw new Error("Only strings are supported at the moment");
+function decodeBencode(bencodedValue: string): string | number {
+  //number
+  if (bencodedValue[0] === "i") {
+    return parseInt(bencodedValue.substring(1, bencodedValue.length - 1));
+  } else if (!isNaN(parseInt(bencodedValue[0]))) {
+    const firstColonIndex = bencodedValue.indexOf(":");
+    if (firstColonIndex === -1) {
+      throw new Error("Invalid encoded value");
     }
+    return bencodedValue.substring(firstColonIndex + 1);
+  } else {
+    throw new Error("Only strings are supported at the moment");
+  }
 }
 
 const args = process.argv;
 const bencodedValue = args[3];
 
 if (args[2] === "decode") {
-    try{
-        const decode = decodeBencode(bencodedValue)
-        console.log(JSON.stringify(decode))
-    }catch{
-        console.error("error decode");
-    }
+  try {
+    const decode = decodeBencode(bencodedValue);
+    console.log(JSON.stringify(decode));
+  } catch {
+    console.error("error decode");
+  }
 }
