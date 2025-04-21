532ae91bd6394b917d19eab4c94626fb0685c053
AlexPet <alexandrpet195@gmail.com>
2025-04-21 18:58:15 +0300

    AH1

--- app/main.ts
+++ app/main.ts
@@ -1,19 +1,39 @@
 // Examples:
 // - decodeBencode("5:hello") -> "hello"
 // - decodeBencode("10:hello12345") -> "hello12345"
-function decodeBencode(bencodedValue: string): string | number {
-  //number
-  if (bencodedValue[0] === "i") {
-    return parseInt(bencodedValue.substring(1, bencodedValue.length - 1));
-  }
-  if (!isNaN(parseInt(bencodedValue[0]))) {
-    const firstColonIndex = bencodedValue.indexOf(":");
-    if (firstColonIndex === -1) {
-      throw new Error("Invalid encoded value");
-    }
-    return bencodedValue.substring(firstColonIndex + 1);
-  } else {
-    throw new Error("Only strings are supported at the moment");
+function decodeBencode(
+  bencodedValue: string
+): string | number | (string | number)[] {
+  switch (bencodedValue[0]) {
+    case "i":
+      return parseInt(bencodedValue.substring(1, bencodedValue.length - 1));
+    case "l":
+      const list: (string | number)[] = [];
+      let index = 1;
+      while (bencodedValue[index] !== "e") {
+        if (bencodedValue[index] === "i") {
+          const end = bencodedValue.indexOf("e", index);
+          list.push(parseInt(bencodedValue.substring(index + 1, end)));
+          index = end + 1;
+        } else {
+          const colon = bencodedValue.indexOf(":", index);
+          const length = parseInt(bencodedValue.substring(index, colon));
+          index = colon + 1;
+          list.push(bencodedValue.substring(index, index + length));
+          index += length;
+        }
+      }
+      return list;
+    default:
+      if (!isNaN(parseInt(bencodedValue[0]))) {
+        const firstColonIndex = bencodedValue.indexOf(":");
+        if (firstColonIndex === -1) {
+          throw new Error("Invalid encoded value");
+        }
+        return bencodedValue.substring(firstColonIndex + 1);
+      } else {
+        throw new Error("Only strings are supported at the moment");
+      }
   }
 }
 
@@ -28,4 +48,3 @@ if (args[2] === "decode") {
     console.error("error decode");
   }
 }
-//test message
