96fd56ce52b74350769e50d9c1c43f973bf706db
AlexPet <alexandrpet195@gmail.com>
2025-04-23 12:40:07 +0300

    OW9

--- app/main.ts
+++ app/main.ts
@@ -1,3 +1,5 @@
+import {readFileSync} from "fs";
+
 function decodeBencode(bencodedValue: string): any {
   function parse(index: number): [any, number] | any {
     const char = bencodedValue[index];
@@ -53,13 +55,26 @@ function decodeBencode(bencodedValue: string): any {
 }
 
 const args = process.argv;
-const bencodedValue = args[3];
+const command = args[2];
+const input = args[3]
 
-if (args[2] === "decode") {
+if (command === "decode") {
   try {
-    const decode = decodeBencode(bencodedValue);
+    const decode = decodeBencode(input);
     console.log(JSON.stringify(decode));
   } catch {
     console.error("error decode");
+  }
+}else if (command === "info"){
+  try {
+    const file = readFileSync(input).toString("binary")
+    const torrent = decodeBencode(file)
+    const anounce = torrent["announce"]
+    const length = torrent["info"]?.["length"];
+
+    console.log(`Tracker URL: ${anounce}`);
+    console.log(`Length: ${length}`);
+  }catch{
+    console.error("error info")
   }
 }
